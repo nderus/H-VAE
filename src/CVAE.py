61c1227fcbe896cb55e912047604ed3c00612d20
Nicolas Derus <99331278+nderus@users.noreply.github.com>
2022-11-28 16:02:00 +0100

    Update CVAE.py

--- src/CVAE.py
+++ src/CVAE.py
@@ -360,3 +360,145 @@ class SecondStage(CVAE):
         u_cond = self.sampling(mu_u, logsd_u, input_label)
         z_hat = self.decoder2(u_cond)
         return z_hat
+    
+ #############
+
+class CVAE_filters(keras.Model):
+    def __init__(self, encoder, decoder, beta, shape, category_count, **kwargs):
+        super(CVAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.beta = beta
+        self.shape = shape
+        self.category_count = category_count
+        self.total_loss_tracker = keras.metrics.Mean(name="loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+        self.total_loss_no_weights_tracker = keras.metrics.Mean(name="loss_no_weights")
+        #
+        self.val_total_loss_tracker = keras.metrics.Mean(name="val_loss")
+        self.val_reconstruction_loss_tracker = keras.metrics.Mean(
+            name="val_reconstruction_loss")
+        self.val_kl_loss_tracker = keras.metrics.Mean(name="val_kl_loss")
+        self.val_total_loss_no_weights_tracker = keras.metrics.Mean(name="val_loss_no_weights")
+
+    
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+            self.val_total_loss_tracker,
+            self.val_reconstruction_loss_tracker,
+            self.val_kl_loss_tracker,
+            self.total_loss_no_weights_tracker,
+            self.val_total_loss_no_weights_tracker,
+        ]
+       
+    def call(self, inputs):
+        _, input_label, conditional_input = self.conditional_input(inputs)
+        z_mean, z_log_var = self.encoder(conditional_input)
+        z_cond = self.sampling(z_mean, z_log_var, input_label)
+        return self.decoder(z_cond)
+
+    def conditional_input(self, inputs):
+        image_size = [self.shape[0], self.shape[1], self.shape[2]]
+        input_img = layers.InputLayer(input_shape=image_size,
+                                    dtype ='float32')(inputs[0])
+        input_label = layers.InputLayer(input_shape=(self.category_count, ),
+                                        dtype ='float32')(inputs[1])
+        labels = tf.reshape(inputs[1], [-1, 1, 1, self.category_count])
+        labels = tf.cast(labels, dtype='float32')
+        ones = tf.ones([inputs[0].shape[0]] + image_size[0:-1] + [self.category_count])
+       # ones = tf.ones([100] + image_size[0:-1] + [self.category_count])
+        labels = ones * labels
+
+        conditional_input = layers.Concatenate(axis=3)([input_img, labels]) 
+        return  input_img, input_label, conditional_input
+    
+
+    def sampling(self, z_mean, z_log_var, input_label):
+        if len(input_label.shape) == 1:
+            input_label = np.expand_dims(input_label, axis=0)
+
+        eps = tf.random.normal(tf.shape(z_log_var), dtype=tf.float32,
+                               mean=0., stddev=1.0, name='epsilon')
+        z = z_mean + tf.exp(z_log_var / 2) * eps
+        image_size = [tf.shape(z_log_var)[1], tf.shape(z_log_var)[2], tf.shape(z_log_var)[3]]
+        labels = tf.reshape(input_label, [-1, 1, 1, self.category_count])
+        labels = tf.cast(labels, dtype='float32')
+        ones = tf.ones([tf.shape(z_log_var)[0] + image_size[0:-1] + [self.category_count])
+        input_label = ones * labels
+        z_cond = tf.concat([z, input_label], axis=1)
+        return z_cond
+
+    @tf.function
+    def train_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+        with tf.GradientTape() as tape:
+            input_img, input_label, conditional_input = self.conditional_input(data)
+            z_mean, z_log_var = self.encoder(conditional_input)
+            z_cond = self.sampling(z_mean, z_log_var, input_label)
+            reconstruction = self.decoder(z_cond)
+
+            reconstruction_loss = tf.reduce_sum(
+                 keras.losses.MSE(input_img, # removed np.prod(self.shape) *
+                                    reconstruction), axis=(1, 2))            
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean)
+                      - tf.exp(z_log_var))
+
+            kl_loss = tf.reduce_sum(kl_loss, axis=1) 
+            total_loss_no_weights = reconstruction_loss + kl_loss
+            total_loss_no_weights = tf.reduce_mean(total_loss_no_weights)
+            kl_loss = self.beta * kl_loss
+            total_loss = reconstruction_loss + kl_loss
+            total_loss = tf.reduce_mean(total_loss) 
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        self.total_loss_no_weights_tracker.update_state(total_loss_no_weights)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+            "loss_no_weights": self.total_loss_no_weights_tracker.result(),
+
+        }
+
+    @tf.function
+    def test_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+        
+        input_img, input_label, conditional_input = self.conditional_input(data)
+        z_mean, z_log_var = self.encoder(conditional_input)
+        z_cond = self.sampling(z_mean, z_log_var, input_label)
+        reconstruction = self.decoder(z_cond)
+        reconstruction_loss = tf.reduce_sum(
+                 keras.losses.MSE(input_img,
+                                    reconstruction), axis=(1, 2))   
+        kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean)
+                  - tf.exp(z_log_var))
+        kl_loss = tf.reduce_sum(kl_loss, axis=1)
+        total_loss_no_weights = reconstruction_loss + kl_loss
+        total_loss_no_weights = tf.reduce_mean(total_loss_no_weights)
+        kl_loss = self.beta * kl_loss
+        total_loss =  reconstruction_loss + kl_loss
+        total_loss = tf.reduce_mean(total_loss)
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        self.total_loss_no_weights_tracker.update_state(total_loss_no_weights)
+        return{
+            'loss': self.total_loss_tracker.result(),
+            'reconstruction_loss': self.reconstruction_loss_tracker.result(),
+            'kl_loss': self.kl_loss_tracker.result(),
+            "loss_no_weights": self.total_loss_no_weights_tracker.result()
+        }
+
