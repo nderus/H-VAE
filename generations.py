31f2456ac3fc3bb71e8a879e2970a6f4db17b6b3
Nicolas Derus <99331278+nderus@users.noreply.github.com>
2022-11-29 17:38:26 +0100

    Update generations.py

--- generations.py
+++ generations.py
@@ -172,7 +172,7 @@ class Generations_filters:
             digit_label_one_hot = to_categorical(digit_label, self.category_count).reshape(1,-1)
             a = tf.convert_to_tensor(digit_label_one_hot)
             b = tf.concat([a, a], axis=0) # with 1 dimension, it fails...
-            z_cond = self.sampling(z_mean=tf.zeros([24, 24, 3]), z_log_var=tf.zeros([24, 24, 3]), input_label = b) # TO DO: sub this with the sampling CVAE function
+            z_cond = self.sampling(z_mean=tf.zeros([2, 24, 24, 3]), z_log_var=tf.zeros([2, 24, 24, 3]), input_label = b) # TO DO: sub this with the sampling CVAE function
             if self.second_stage:
                 z_cond = self.model2.posterior(z_cond, b)
             decoded_x = self.model.decoder.predict(z_cond)
