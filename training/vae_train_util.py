9e3b907b023c158fea60032ca23ea58d84de72b3
Nicolas Derus <99331278+nderus@users.noreply.github.com>
2023-02-03 17:47:58 +0100

    Update vae_train_util.py

--- training/vae_train_util.py
+++ training/vae_train_util.py
@@ -1,1 +1,15 @@
-
+def vae_defaults():
+    """
+    Defaults for VAE models.
+    """
+    return dict(
+        dataset_name = 'histo',
+        model_name = 'CVAE_resnet_l2',
+        kl_coefficient = .03,
+        encoded_dim = 1024 + 2048,
+        learning_rate = 0.0001,
+        epoch_count = 100,
+        batch_size = 100,
+        patience = 10,
+        wandb_logging = False,
+    )
