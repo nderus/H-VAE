1051722eb2eec9bcc054728601735f498d2d7081
Nicolas Derus <99331278+nderus@users.noreply.github.com>
2023-02-03 18:11:38 +0100

    Update vae_train_utils.py

--- training/vae_train_utils.py
+++ training/vae_train_utils.py
@@ -13,3 +13,12 @@ def vae_defaults():
         patience = 10,
         wandb_logging = False,
     )
+
+def add_dict_to_argparser(parser, default_dict):
+    for k, v in default_dict.items():
+        v_type = type(v)
+        if v is None:
+            v_type = str
+        elif isinstance(v, bool):
+            v_type = str2bool
+        parser.add_argument(f"--{k}", default=v, type=v_type)
