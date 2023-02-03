e760bda884c6bfa6023a34b5343631d363ddbb2b
Nicolas Derus <99331278+nderus@users.noreply.github.com>
2023-02-03 18:35:04 +0100

    Create vae_train.py

---
+++ training/vae_train.py
@@ -0,0 +1,34 @@
+"""
+Train a VAE model on images.
+"""
+import argparse
+
+from training.vae_train_utils import vae_defaults
+from training.vae_train_utils import add_dict_to_argparser
+from training.vae_train_utils import args_to_dict
+
+
+def main():
+  
+  args = create_argparser().parse_args()
+  args_to_dict(args, vae_defaults().keys())
+  
+def create_argparser():
+    defaults = dict(
+        dataset_name = 'histo',
+        model_name = 'CVAE_resnet_l2',
+        kl_coefficient = .03,
+        encoded_dim = 1024 + 2048,
+        learning_rate = 0.0001,
+        epoch_count = 100,
+        batch_size = 100,
+        patience = 10,
+        wandb_logging = False,
+    )
+    defaults.update(vae_defaults())
+    parser = argparse.ArgumentParser()
+    add_dict_to_argparser(parser, defaults)
+    return parser
+
+if __name__ == "__main__":
+  main()
