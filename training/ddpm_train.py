5be8263c58db263105dbed6275ff0c6cb0217158
nderus <nicolas.derus2@unibo.it>
2023-02-06 19:09:49 +0100

    ddpm train fix

--- training/ddpm_train.py
+++ training/ddpm_train.py
@@ -7,7 +7,7 @@ import tensorflow_datasets as tfds
 from tensorflow import keras
 from keras import layers
 
-from src.models.DDPM import DiffusionModel
+from src.models.DDPM.DDPM import DiffusionModel
 from training.ddpm_train_utils import ddpm_defaults
 from training.ddpm_train_utils import add_dict_to_argparser
 from training.ddpm_train_utils import args_to_dict
@@ -19,9 +19,10 @@ def main():
     args = create_argparser().parse_args()
     builder = tfds.builder_from_directory('/datasets/=//histo/1.0.0')
     # Construct the tf.data.Dataset pipeline
-    train_ds, val_ds = builder.as_dataset(split=  ["cancer[:60000]",
+    train_ds, val_ds = builder.as_dataset(split = ["cancer[:60000]",
                                                   "cancer[60000:70000]"],
                                                   as_supervised=False, shuffle_files=False)
+    
     train_ds = train_ds.map(preprocess_image, num_parallel_calls = tf.data.AUTOTUNE).cache()
     train_ds = train_ds.repeat(args.dataset_repetitions).shuffle(10 * args.batch_size)
     train_ds = train_ds.batch(args.batch_size, drop_remainder = True).prefetch(buffer_size = tf.data.AUTOTUNE)
@@ -29,9 +30,21 @@ def main():
     val_ds = val_ds.repeat(args.dataset_repetitions).shuffle(10 * args.batch_size)
     val_ds = val_ds.batch(args.batch_size, drop_remainder = True).prefetch(buffer_size = tf.data.AUTOTUNE)
 
-    # create and compile the model
-    model = DiffusionModel(args.image_size, args.widths, args.block_depth)
 
+    
+    # create and compile the model
+    model = DiffusionModel(image_size = args.image_size,
+                           widths = args.widths, 
+                           block_depth = args.block_depth, 
+                           embedding_max_frequency = args.embedding_max_frequency,
+                           embedding_dims =args.embedding_dims,
+                           batch_size = args.batch_size,
+                           min_signal_rate = args.min_signal_rate, 
+                           max_signal_rate = args.max_signal_rate, 
+                           ema = args.ema, 
+                           kid_diffusion_steps = args.kid_diffusion_steps, 
+                           plot_diffusion_steps = args.plot_diffusion_steps,
+                           kid_image_size = args.kid_image_size)
     model.compile(
         optimizer=keras.optimizers.Adam(
             learning_rate=args.learning_rate,
