b42b5aeeeaaa431830ef1a3e066be90dd1153344
nderus <nicolas.derus2@unibo.it>
2023-02-06 15:45:51 +0100

    reorganized folders

---
+++ training/ddpm_train.py
@@ -0,0 +1,9 @@
+import math
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from tensorflow import keras
+from keras import layers
+
+#!pip install -q tfds-nightly
